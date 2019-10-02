395c3abe19b92c33df5dea5bbe92a1e71feef901
Francisco Albarran Bravo <pakoalb@hotmail.com>
2019-10-02 11:17:44 -0500

    Primer commit

---
+++ src/data.js
@@ -0,0 +1,103 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "T-Shirt Kings ",
+        img: "product_images/adoration-of-the-kings.gif",
+        price: 10,
+        company: "FAB",
+        info:"Lorem ipsum",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "T-Shirt Afghan ",
+        img: "product_images/afghan-flower.gif",
+        price: 20,
+        company: "FAB",
+        info:"Lorem ipsum",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "T-Shirt Albania ",
+        img: "product_images/albania-flower.gif",
+        price: 14,
+        company: "FAB",
+        info:"Lorem ipsum",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "T-Shirt Alsace ",
+        img: "product_images/alsace.gif",
+        price: 14.6,
+        company: "FAB",
+        info:"Información",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "T-Shirt Altar ",
+        img: "product_images/altar-piece.gif",
+        price: 11,
+        company: "FAB",
+        info:"Construcción",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "T-Shirt Partridge ",
+        img: "product_images/a-partridge-in-a-pear-tree.gif",
+        price: 13,
+        company: "FAB",
+        info:"???????",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "T-Shirt Apocalypse ",
+        img: "product_images/apocalypse-tapestry.gif",
+        price: 18,
+        company: "FAB",
+        info:"Construcción",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "T-Shirt Arc Triomphe ",
+        img: "product_images/arc-d-triomphe.gif",
+        price: 20,
+        company: "FAB",
+        info:"???????",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "T-Shirt Kings ",
+    img: "product_images/adoration-of-the-kings.gif",
+    price: 10,
+    company: "FAB",
+    info:"Lorem ipsum",
+    inCart: false,
+    count: 0,
+    total: 0
+}
+
